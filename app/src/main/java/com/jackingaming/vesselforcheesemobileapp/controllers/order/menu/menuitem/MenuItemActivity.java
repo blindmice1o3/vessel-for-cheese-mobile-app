dca8263538aa76f5d655a69a0d9a933fd700b7e7
jamesgov <blindmice1o3@gmail.com>
2023-05-30 12:03:18 -0700

    feat: Trigger toolbar title when collapsed
    Used AppBarLayout.OnOffsetChangedListener to set a title on the CollapsingToolbarLayout when it is collapsed, and un-set the title for all other verticalOffset.
    
    MenuItemActivity's collapsing toolbar is now like the siren's mobile app's detail activity's toolbar.

--- app/src/main/java/com/jackingaming/vesselforcheesemobileapp/controllers/order/menu/menuitem/MenuItemActivity.java
+++ app/src/main/java/com/jackingaming/vesselforcheesemobileapp/controllers/order/menu/menuitem/MenuItemActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuInflater;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.jackingaming.vesselforcheesemobileapp.R;
 import com.jackingaming.vesselforcheesemobileapp.models.menu.Menu;
@@ -25,10 +28,16 @@ public class MenuItemActivity extends AppCompatActivity {
         Log.i(TAG, "onCreate()");
         setContentView(R.layout.activity_menu_item);
 
-        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_menu_close_clear_cancel);
         setSupportActionBar(toolbar);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
+        collapsingToolbarLayout.setTitle(" ");
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar_layout);
+        ImageView ivMenuItemImage = (ImageView) findViewById(R.id.iv_menuitem_image);
+        TextView tvMenuItemName = (TextView) findViewById(R.id.tv_menuitem_name);
+        TextView tvMenuItemCalories = (TextView) findViewById(R.id.tv_menuitem_calories);
 
         String nameCategory = getIntent().getStringExtra(EXTRA_NAME_CATEGORY);
         String nameSubCategory = getIntent().getStringExtra(EXTRA_NAME_SUB_CATEGORY);
@@ -41,10 +50,50 @@ public class MenuItemActivity extends AppCompatActivity {
             MenuItem menuItem = Menu.hotCoffeesAsMap.get(nameSubCategory).get(position);
             String textPrevious = tvContent.getText().toString();
             tvContent.setText(textPrevious + ": " + menuItem.getName());
-            collapsingToolbarLayout.setTitle(menuItem.getName());
+
+            // TODO: add field to MenuItem class: long idImageResource.
+            // TODO: add field to MenuItem class: int calories.
+            ivMenuItemImage.setImageResource(R.drawable.harvest_moon_natsume);
+            tvMenuItemName.setText(menuItem.getName());
+            tvMenuItemCalories.setText("42 calories");
+
+            appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+                @Override
+                public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                    if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()) {
+                        // Collapsed
+                        collapsingToolbarLayout.setTitle(menuItem.getName());
+                    } else {
+                        // Un-collapsed
+                        // careful: there should a space between double quote otherwise it wont work.
+                        collapsingToolbarLayout.setTitle(" ");
+                    }
+
+//                    float percentage = (float) Math.abs(verticalOffset) / appBarLayout.getTotalScrollRange();
+//                    if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()) {
+//                        // Collapsed
+//                        collapsingToolbarLayout.setTitle(menuItem.getName());
+//                    } else if (verticalOffset == 0) {
+//                        // Expanded
+//                        // careful: there should a space between double quote otherwise it wont work.
+//                        collapsingToolbarLayout.setTitle(" ");
+//                    } else {
+//                        // In-between
+//                        collapsingToolbarLayout.animate().alpha(percentage);
+//                    }
+                }
+            });
         } else {
             Log.e(TAG, "nameCategory does NOT equals " + Menu.HOT_COFFEES);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        Log.i(TAG, "onCreateOptionsMenu()");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options_menu_item_activity, menu);
+        return true;
     }
 
     @Override
@@ -52,10 +101,12 @@ public class MenuItemActivity extends AppCompatActivity {
         Log.i(TAG, "onOptionsItemSelected()");
 
         if (item.getItemId() == android.R.id.home) {
-            Log.d(TAG, "android.R.id.home");
+            Log.i(TAG, "android.R.id.home");
             finish();
+        } else if (item.getItemId() == R.id.menu_item_info) {
+            Log.i(TAG, "R.id.menu_item_info");
         } else {
-            Log.d(TAG, "NOT android.R.id.home");
+            Log.i(TAG, "NOT android.R.id.home or R.id.menu_item_info");
         }
         return super.onOptionsItemSelected(item);
     }
