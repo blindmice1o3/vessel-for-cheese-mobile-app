281f7b6bd826276cb1795ade7136d6c28d07f3f6
jamesgov <blindmice1o3@gmail.com>
2023-08-17 22:18:01 -0700

    tinker: Define recipe for all* IcedCoffees
    *Except for [IcedEspresso].

--- app/src/main/java/com/jackingaming/vesselforcheesemobileapp/models/menu_items/drinks/brewed/cold/icedcoffees/IcedCoffeeWithMilk.java
+++ app/src/main/java/com/jackingaming/vesselforcheesemobileapp/models/menu_items/drinks/brewed/cold/icedcoffees/IcedCoffeeWithMilk.java
@@ -1,6 +1,12 @@
 package com.jackingaming.vesselforcheesemobileapp.models.menu_items.drinks.brewed.cold.icedcoffees;
 
 import com.jackingaming.vesselforcheesemobileapp.R;
+import com.jackingaming.vesselforcheesemobileapp.models.components.drinks.DrinkComponentWithDefaultAsString;
+import com.jackingaming.vesselforcheesemobileapp.models.components.drinks.milk_options.MilkBase;
+import com.jackingaming.vesselforcheesemobileapp.models.components.drinks.milk_options.MilkOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class IcedCoffeeWithMilk extends IcedCoffees {
     public static final String TAG = IcedCoffeeWithMilk.class.getSimpleName();
@@ -12,6 +18,8 @@ public class IcedCoffeeWithMilk extends IcedCoffees {
     public static final int DEFAULT_SUGAR_IN_GRAM = 23;
     public static final float DEFAULT_FAT_IN_GRAM = 1.5f;
 
+    public static final MilkBase.Type DEFAULT_MILK_BASE = MilkBase.Type.TWO_PERCENT;
+
     public static final double DEFAULT_PRICE_SMALL = 2.95;
     public static final double DEFAULT_PRICE_MEDIUM = 3.45;
     public static final double DEFAULT_PRICE_LARGE = 3.70;
@@ -20,5 +28,16 @@ public class IcedCoffeeWithMilk extends IcedCoffees {
         super(DEFAULT_IMAGE_RESOURCE_ID, DEFAULT_NAME, DEFAULT_DESCRIPTION,
                 DEFAULT_CALORIES, DEFAULT_SUGAR_IN_GRAM, DEFAULT_FAT_IN_GRAM,
                 DEFAULT_PRICE_MEDIUM);
+
+        // MILK_OPTIONS (add into NEW DrinkComponent group)
+        MilkBase milkBaseTwoPercent = new MilkBase(DEFAULT_MILK_BASE);
+
+        List<DrinkComponentWithDefaultAsString> milkOptions = new ArrayList<>();
+        milkOptions.add(new DrinkComponentWithDefaultAsString(
+                milkBaseTwoPercent, DEFAULT_MILK_BASE.name()
+        ));
+
+        drinkComponents.put(MilkOptions.TAG, milkOptions);
+        drinkComponentsStandardRecipe.add(milkBaseTwoPercent);
     }
 }
--- app/src/main/java/com/jackingaming/vesselforcheesemobileapp/models/menu_items/drinks/brewed/cold/icedcoffees/IcedCoffees.java
+++ app/src/main/java/com/jackingaming/vesselforcheesemobileapp/models/menu_items/drinks/brewed/cold/icedcoffees/IcedCoffees.java
@@ -1,9 +1,16 @@
 package com.jackingaming.vesselforcheesemobileapp.models.menu_items.drinks.brewed.cold.icedcoffees;
 
+import com.jackingaming.vesselforcheesemobileapp.models.components.drinks.DrinkComponentWithDefaultAsString;
+import com.jackingaming.vesselforcheesemobileapp.models.components.drinks.sweetener_options.Liquid;
+import com.jackingaming.vesselforcheesemobileapp.models.components.drinks.sweetener_options.SweetenerOptions;
 import com.jackingaming.vesselforcheesemobileapp.models.menu_items.drinks.brewed.cold.ColdBrewedCoffees;
+
+import java.util.List;
 
 public abstract class IcedCoffees extends ColdBrewedCoffees {
     public static final String TAG = IcedCoffees.class.getSimpleName();
+
+    public static final Liquid.Type DEFAULT_LIQUID_CLASSIC = Liquid.Type.CLASSIC;
 
     public IcedCoffees() {
     }
@@ -14,5 +21,16 @@ public abstract class IcedCoffees extends ColdBrewedCoffees {
         super(imageResourceId, name, description,
                 calories, sugarInGram, fatInGram,
                 price);
+
+        // SWEETENER_OPTIONS (add into EXISTING DrinkComponent group)
+        int numberOfPumpByDrinkSize = getNumberOfPumpByDrinkSize(drinkSize);
+        Liquid liquidClassic = new Liquid(DEFAULT_LIQUID_CLASSIC, numberOfPumpByDrinkSize);
+
+        List<DrinkComponentWithDefaultAsString> sweetenerOptions = drinkComponents.get(SweetenerOptions.TAG);
+        sweetenerOptions.add(0, new DrinkComponentWithDefaultAsString(
+                liquidClassic, Integer.toString(numberOfPumpByDrinkSize)
+        ));
+
+        drinkComponentsStandardRecipe.add(liquidClassic);
     }
 }
